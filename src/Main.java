376733253562ad1a2350d544354a8059b5ea45c6
Cucumber98 <ilusha.a1998@mail.ru>
2025-05-07 23:26:14 +0300

    немного посчитал

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("HI!");
+        System.out.println("Привет!");
 
         for (int i = 1; i <= 5; i++) {
             System.out.println("i = "+i);
