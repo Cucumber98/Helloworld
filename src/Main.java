ae87f7348db9bc9f235ec6eedaf732ea45a9de15
Cucumber98 <ilusha.a1998@mail.ru>
2025-05-07 23:27:09 +0300

    немного посчитал

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,6 @@ public class Main {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.println("Привет!");
-
         for (int i = 1; i <= 5; i++) {
             System.out.println("i = "+i);
             System.out.println("i^2 = "+i*i);
